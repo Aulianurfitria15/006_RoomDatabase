c7f155f3ef8c110e13d04ca06baffafd458aa46f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-28 18:49:36 +0700

    menambahkan interface DestinasiNavigasi sebagai dasar sistem navigasi

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiNavigasi.kt
@@ -1,4 +1,9 @@
 package com.example.roomdatabase.view.route
 
 interface DestinasiNavigasi {
+    //nama unutk mnentukan jalur unutuk composable
+    val route: String
+
+    //string resource id yang berisi judul yang akan ditampilkan di layar halaman
+    val titleRes: Int
 }
