2085f6973848ac3c5f34e97399dac798d650a441
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-28 15:52:55 +0700

    membuat class homeviewmodel

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    companion object{
+    }
+}
