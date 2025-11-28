ecf3bd00724276bd85c4e6f853a5141a5f5ad182
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-28 18:57:01 +0700

    Menambahkan dataclass UIStateSiswa dan detail siswa

--- app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -29,3 +29,22 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() 
     }
 }
 
+
+        data class UIStateSiswa(
+            val detailSiswa: DetailSiswa = DetailSiswa(),
+            val isEntryValid: Boolean = false
+        )
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String =""
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
